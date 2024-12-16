3e5c8130c22dc5c4d93782cf403278334f7b9d50
Sunny Raj <153062846+rajSunny5714@users.noreply.github.com>
2024-12-16 10:19:50 +0530

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,20 @@
+public class Hello{
+    public static void main(String args[]) {
+        System.out.println("Hello World");
+
+        int a = 1;
+        System.out.println(Integer.toString(a));
+
+        String name = "Sunny Raj";
+        System.out.println(name);
+
+        for(int i=1; i<10; i++) {
+            System.out.println(i);
+        }
+
+        int i= 0;
+        while(i++<10){
+            System.out.println("S");
+        }
+    }
+}
