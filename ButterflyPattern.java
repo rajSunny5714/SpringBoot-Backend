b523ce313c14c17d8d063e51b451f2680405fa49
Sunny Raj <153062846+rajSunny5714@users.noreply.github.com>
2024-12-18 09:32:52 +0530

    Add files via upload

---
+++ ButterflyPattern.java
@@ -0,0 +1,39 @@
+public class ButterflyPattern {
+        public static void main(String[] args) {
+            int n = 5;
+    
+            // Upper half of the diamond
+            for (int i = 1; i  <= n; i++) {
+                // Print left half of the row
+                for (int j = 1; j  <= i; j++) {
+                    System.out.print("*");
+                }
+                // Print spaces in the middle
+                for (int j = 1; j  <= 2 * (n - i); j++) {
+                    System.out.print(" ");
+                }
+                // Print right half of the row
+                for (int j = 1; j  <= i; j++) {
+                    System.out.print("*");
+                }
+                System.out.println();
+            }
+    
+            // Lower half of the diamond
+            for (int i = n; i  >= 1; i--) {
+                // Print left half of the row
+                for (int j = 1; j  <= i; j++) {
+                    System.out.print("*");
+                }
+                // Print spaces in the middle
+                for (int j = 1; j  <= 2 * (n - i); j++) {
+                    System.out.print(" ");
+                }
+                // Print right half of the row
+                for (int j = 1; j  <= i; j++) {
+                    System.out.print("*");
+                }
+                System.out.println();
+            }
+        }
+    }
